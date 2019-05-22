09cdf7515765de1ec6a2f51ee7930c71e1aff3b8
Unknown <tremblay.eti@gmail.com>
2019-05-22 18:19:42 -0400

    MobileNetV2

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from keras.applications.vgg16 import VGG16
+from keras.applications.mobilenetv2 import MobileNetV2
 from keras.layers import Input, Lambda, Dense, Dropout
 from keras.models import Model
 from keras import backend as K
@@ -7,24 +7,29 @@ from keras.optimizers import Adam
 from keras.layers.normalization import BatchNormalization
 import os
 
-input_shape = (4096,)
 weights_path = os.path.dirname(__file__) + '/model_weights.h5'
 
 
 def get_encoder():
-    vgg16_model = VGG16(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
-    model = models.Sequential()
-    for layer in vgg16_model.layers[:-2]:  # exclude some layers
-        model.add(layer)
+    input_tensor = Input(shape=(224, 224, 3))
+    model = MobileNetV2(
+        include_top=False,
+        weights='imagenet',
+        input_tensor=input_tensor,
+        input_shape=(224, 224, 3),
+        pooling='avg')
     return model
 
 
+latent_shape = (get_encoder().output_shape[1],)
+
+
 def get_siamese(load_weights=True):
-    left_input = Input(input_shape)
-    right_input = Input(input_shape)
+    left_input = Input(latent_shape)
+    right_input = Input(latent_shape)
 
     # merge two encoded inputs with the l1 distance between them
-    L1_layer = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]), output_shape=(4096,))
+    L1_layer = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]), output_shape=latent_shape)
     L1_distance = L1_layer([left_input, right_input])
     normalised_distance = BatchNormalization()(L1_distance)
     prediction = Dense(1, activation='sigmoid')(normalised_distance)
