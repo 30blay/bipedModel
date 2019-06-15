bc3c26aa9a0c3a516ec9968f8ce76ccb46a65b1f
Unknown <tremblay.eti@gmail.com>
2019-06-15 18:14:30 -0400

    extract_features
    Add function to extract features

--- model.py
+++ model.py
@@ -2,21 +2,21 @@ from keras.applications.mobilenetv2 import MobileNetV2
 from keras.layers import Input, Lambda, Dense, Dropout
 from keras.models import Model
 from keras import backend as K
-from keras import models
 from keras.optimizers import Adam
 from keras.layers.normalization import BatchNormalization
 import os
 
 weights_path = os.path.dirname(__file__) + '/model_weights.h5'
+input_shape = (224, 224, 3)
 
 
 def get_encoder():
-    input_tensor = Input(shape=(224, 224, 3))
+    input_tensor = Input(shape=input_shape)
     model = MobileNetV2(
         include_top=False,
         weights='imagenet',
         input_tensor=input_tensor,
-        input_shape=(224, 224, 3),
+        input_shape=input_shape,
         pooling='avg')
     return model
 
@@ -44,3 +44,22 @@ def get_siamese(load_weights=True):
         siamese_net.load_weights(weights_path)
 
     return siamese_net
+
+
+def extract_features(img_path):
+    from keras.preprocessing import image
+    from keras.preprocessing.image import ImageDataGenerator
+    import numpy as np
+
+    encoder = get_encoder()
+
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    img = image.load_img(img_path, target_size=input_shape[0:2])
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    generator = datagen.flow(x)
+
+    for inputs_batch in generator:
+        features_batch = encoder.predict(inputs_batch)
+        return features_batch
