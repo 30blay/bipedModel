0916f09435befa716751442164de6d44e2a047b2
etienne <tremblay.etienne@gmail.com>
2019-10-03 12:38:51 -0400

    Fix errors

--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 import skimage.segmentation as seg
 from skimage import io
 from skimage.transform import resize
+from skimage.color import rgb2hsv
 import numpy as np
 from PIL import Image
+from scipy.spatial.distance import cosine as cosine_distance
+import matplotlib.pyplot as plt
 
 
 class BipedModel:
@@ -11,33 +14,45 @@ class BipedModel:
 
     def extract_features(self, img_path):
         image = io.imread(img_path)
-        mask = self.isolate_sock(image)
+        image = self.resize(image)
+        image, mask = self.isolate_sock(image)
         features = self.histogram(image, mask)
         return features
 
-    def isolate_sock(self, image):
+    def resize(self, image):
         # first resize the image for quicker calculations
         aspect_ratio = image.shape[1] / image.shape[0]
         resized_height = 100
         resized_width = int(resized_height * aspect_ratio)
-        image = resize(image, (resized_height, resized_width), anti_aliasing=False)
+        image = resize(image, (resized_height, resized_width), anti_aliasing=False, preserve_range=True)
+        return image
 
+    def isolate_sock(self, image):
         # low min size factor because we don't want the algo to drop a segment
-        mask = seg.slic(image, n_segments=2, min_size_factor=0.01, max_size_factor=2, compactness=4)
+        mask = seg.slic(image, n_segments=2, min_size_factor=0.01, max_size_factor=2, compactness=10)
         image = image * np.repeat(mask[:, :, np.newaxis], 3, axis=2)
-
-        # make a 1D vector with all the unmasked pixels
-        #sock = image[np.nonzero(image.mean(axis=2))]
-        t2 = time.process_time()
-        return mask
+        image = image.astype(int)
+        #plt.imshow(image)
+        #plt.show()
+        return image, mask
 
     def get_similarity(self, feature1, feature2):
-        distance = np.linalg.norm(feature1, feature2)
-        similarity = 1-distance
+        similarity = 1-cosine_distance(feature1, feature2)
         return similarity
 
-    def histogram(self, image, mask):
-        pil_image = Image.fromarray(image)
-        count_histogram = pil_image.histogram()
+    def histogram(self, image, mask, method='pil_histogram'):
+        # make a linear image with all the unmasked pixels
+        sock = image[np.nonzero(image.mean(axis=2))]
+        sock = sock[:, np.newaxis, :]
+
+        if method == 'pil_histogram':
+            sock = np.uint8(sock)
+            pil_image = Image.fromarray(sock)
+            count_histogram = pil_image.histogram()
+
+        if method == 'hue':
+            hsv = rgb2hsv(sock)
+            pass
+
         density_histogram = count_histogram/np.linalg.norm(count_histogram, ord=2, keepdims=True)
         return density_histogram
